6929a6b913f7703573102aefcb03287799f33b20
lauanderson-rael <lauanderson38@gmail.com>
2026-06-04 16:54:34 -0300

    feat: ajuste  na tela de login do admin

--- config/settings.py
+++ config/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME', 'preifma_db'),
         'USER': os.environ.get('DB_USER', 'preifma_user'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'admin123'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
